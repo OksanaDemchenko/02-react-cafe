61635d55110e09ac1f272f1421779b7fbb8313e0
Demchenko Oksana <sulichka33@gmail.com>
2025-11-28 18:56:14 +0200

    fixed files

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './components/App/App.tsx'
-import 'modern-normalize/modern-normalize.css';
+import 'modern-normalize/modern-normalize.css'
+import App from './components/App/App';
 
 
 createRoot(document.getElementById('root')!).render(
